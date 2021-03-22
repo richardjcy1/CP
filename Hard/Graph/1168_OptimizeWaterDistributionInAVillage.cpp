dcbe46927bd26a48fd138e5c0a140781732bd0d0
richardjcy1 <chaoyij@alumni.cmu.edu>
2021-03-22 08:59:40 -0700

    Graph MST 1168

--- Hard/Graph/1168_OptimizeWaterDistributionInAVillage.cpp
+++ Hard/Graph/1168_OptimizeWaterDistributionInAVillage.cpp
@@ -79,7 +79,7 @@ ll Graph::kruskalMST()
 
         if (set_u != set_v)
         {
-            //cout << u << " - " << v << endl;
+            // cout << u << " - " << v << endl;
             mst_wt += it->first;
             ds.uni(set_u, set_v);
             --need;
@@ -90,33 +90,19 @@ ll Graph::kruskalMST()
     return mst_wt;
 }
 
-int main() 
-{ 
-    /* Let us create above shown weighted 
-       and unidrected graph */
-    int V = 9, E = 14; 
-    Graph g(V); 
-  
-    //  making above shown graph 
-    g.addEdge(0, 1, 4); 
-    g.addEdge(0, 7, 8); 
-    g.addEdge(1, 2, 8); 
-    g.addEdge(1, 7, 11); 
-    g.addEdge(2, 3, 7); 
-    g.addEdge(2, 8, 2); 
-    g.addEdge(2, 5, 4); 
-    g.addEdge(3, 4, 9); 
-    g.addEdge(3, 5, 14); 
-    g.addEdge(4, 5, 10); 
-    g.addEdge(5, 6, 2); 
-    g.addEdge(6, 7, 1); 
-    g.addEdge(6, 8, 6); 
-    g.addEdge(7, 8, 7); 
-  
-    cout << "Edges of MST are \n"; 
-    ll mst_wt = g.kruskalMST(); 
-  
-    cout << "\nWeight of MST is " << mst_wt; 
-  
-    return 0; 
-}
+class Solution {
+public:
+    int minCostToSupplyWater(int N, vector<int>& wells, vector<vector<int>>& pipes) {
+        int n = wells.size(), m = pipes.size();
+        Graph g(n + 1);
+        for (int i = 0; i < n; i++)
+        {
+            g.addEdge(0, i + 1, wells[i]);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            g.addEdge(pipes[i][0], pipes[i][1], pipes[i][2]);
+        }
+        return g.kruskalMST();
+    }
+};
