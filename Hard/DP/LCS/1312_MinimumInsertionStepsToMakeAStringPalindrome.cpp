7d3acb8a83c6d7c0f16729a961cd5ced3d98904f
richardjcy1 <chaoyij@alumni.cmu.edu>
2021-03-27 23:23:23 -0700

    DP 10, 44, 1312

---
+++ Hard/DP/LCS/1312_MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n)); 
+        for (int i = 0; i < n - 1; i++) dp[i][i + 1] = (s[i] == s[i + 1]) ? 0 : 1;
+        for (int l = 3; l <= n; l++)
+        {
+            for (int i = 0; i + l <= n; i++)
+            {
+                int j = i + l - 1;
+                if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1];
+                else dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+};
